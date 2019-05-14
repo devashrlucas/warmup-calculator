d2794e741ecac99cd2731b58ddf7ff9f949577d9
devashrlucas <devashrlucas@gmail.com>
2019-05-14 09:55:47 -0400

    Installed ESLint locally

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// load math.js
+import * as math from 'mathjs'
+
+// use math.js
+math.sqrt(-4); // 2i
+
+
+let warmupSetWeights = [45, 45, 58, 71, 84, 100];
+let warmUp = [45, 45, 58, 71, 84, 100];
+let roundedList = [45.0, 45.0, 60.0, 75.0, 85.0, 100.0];
+let halfStack = [0.0, 0.0, 7.5, 15.0, 20.0, 27.5];
+
+console.log(math.sqrt(-4));
