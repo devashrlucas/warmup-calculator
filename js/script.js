74e2d3db0ed57d918661aff5ba4558a24c933e3a
devashrlucas <devashrlucas@gmail.com>
2019-05-20 15:20:45 -0400

    Deleted some files.

--- js/script.js
+++ js/script.js
@@ -1,13 +1,9 @@
-/* JSDOM is useful for testing and scraping via browser emulation -- MAY NOT NEED TO USE THIS
-const jsdom = require('jsdom');
 
-const { JSDOM } = jsdom;
-
+/*
 const math = require('mathjs');
 
-
-let warmupSetWeights = [0, 0, 0, 0, 0, 0]; //based off form data
-let warmUp = [0, 0, 0, 0, 0, 0]; //based off form data
+let warmupSetWeights = [45, 45, 58, 71, 84, 100];
+let warmUp = [45, 45, 58, 71, 84, 100];
 let roundedList = [45.0, 45.0, 60.0, 75.0, 85.0, 100.0];
 let halfStack = [0.0, 0.0, 7.5, 15.0, 20.0, 27.5];
 
@@ -24,7 +20,6 @@ function getInputs() {
     }
 }
 
-*/
 
 /* Push form data into an array
 
@@ -37,13 +32,32 @@ console.log(formData);
 */
 
 function getInputs() {
-var startInput = document.querySelector('#start').innerText;
-  var startText = '';
-  var i;
+  const barInput = document.querySelector('#bar').innerHTML;
+  let barText = '';
+  const startInput = document.querySelector('#start').innerText;
+  let startText = '';
+  const endInput = document.querySelector('#end').innerText;
+  let endText = '';
+  const inputArray = [];
+  let i;
+    for (i = 0; i < barInput.length; i++) {
+        barText += barInput.charAt(i);
+    }
+    inputArray.push(barText);
   for (i = 0; i < startInput.length; i++) {
     startText += startInput.charAt(i);
   }
-  console.log(startText);
+  inputArray.push(startText);
+  for (i = 0; i < endInput.length; i++) {
+    endText += endInput.charAt(i);
+  }
+  inputArray.push(endText);
+  return inputArray;
 }
 
-getInputs(); 
+let inputWeight = getInputs();
+
+function getWarmupSetWeights() {
+  let warmupSetWeights = [];
+  let setEquation = ((inputWeight[1] - inputWeight[0]) / 4);
+}
